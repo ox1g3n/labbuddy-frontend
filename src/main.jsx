dd3f1701ea3b5d769e7cabd8e02ee66c6416f059
ashutosh <ak956901@gmail.com>
2025-05-25 02:50:28 +0530

    logtail

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
+import { initializeLogging } from './utils/logging';
+
+// Initialize logging as early as possible
+initializeLogging();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
